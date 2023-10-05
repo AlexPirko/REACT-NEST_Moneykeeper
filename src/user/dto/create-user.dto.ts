057ed690511e23356864f0e6d176842d763401cc
Alex Pirko <youzhny_foto@gmail.com>
2023-10-05 23:23:53 +0300

    feat: add authorisation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import { IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @MinLength(6, { message: 'Password must be more when 5 symbols' })
+  password: string;
+}
